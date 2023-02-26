4a98b4eb5297a737af0cb634626dc67d4e8f6ac6
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2023-02-26 15:57:53 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -768,7 +768,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         buttons = [[
             InlineKeyboardButton("➕️ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ➕️", url=f"http://t.me/{temp.U_NAME}?startgroup=true")
             ],[
-            InlineKeyboardButton('🔄 Sʜᴀʀᴇ Mᴇ', url='https://telegram.me/share/url?url=t.me/{temp.U_NAME}'), 
+            InlineKeyboardButton('🔄 Sʜᴀʀᴇ Mᴇ', url=f'https://t.me/share/url?url=t.me/{temp.U_NAME}'), 
             InlineKeyboardButton("🤖 Uᴘᴅᴀᴛᴇs", url="https://t.me/muryana_movies")
             ],[      
             InlineKeyboardButton("❗ Hᴇʟᴘ", callback_data="help2"),
