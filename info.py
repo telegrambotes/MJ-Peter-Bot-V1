132da67f7e48958c51ac82af0b07c48b48e1ae06
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2023-02-26 19:27:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -61,7 +61,7 @@ MELCOW_VID = environ.get('MELCOW_VID',"")
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1061576483').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '0').split()]
-auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
+auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '1061576483 1251324232').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('AUTH_CHANNEL')
 auth_grp = environ.get('AUTH_GROUP')
