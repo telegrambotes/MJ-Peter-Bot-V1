cca34d3c64e95dd502ddf61f179ff5ff659c8704
telegrambotes <108186703+telegrambotes@users.noreply.github.com>
2023-03-29 12:55:11 +0530

    Update info.py

--- info.py
+++ info.py
@@ -64,7 +64,7 @@ HOW_DWLD_LINK = environ.get('HOW_DWLD_LINK', 'https://t.me/mj_bypass_link')
 IMDB_LINK = environ.get('IMDB_LINK', 'https://imdb.com/find?q={search}')
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1061576483 1365052525').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1061576483 1365052525 5963138883').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '0').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '1365052525').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
